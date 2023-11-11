033f1fe50d0731005c285f6af7689aeda776b443
nathan_thai <zonarcthedev@gmail.com>
2023-11-11 01:49:27 -0800

    Working simple name and money input with class Player.

--- money_management.py
+++ money_management.py
@@ -3,40 +3,6 @@ print("Welcome to our Totally Not Rigged Casino!")
 print("My name is Nathan and I will be your host this evening")
 print("")
 
-
-# # Collect name
-# name = input("What is your name?: ")
-# print("")
-# print(f"Hello, {name}! ")
-#
-# print("")
-# print("Since our casino is quite new, we can only accept deposit between $100 to $2000")
-#
-# # Collect amount of money deposit that must be a number between 100 and 2000
-# while True:
-#     try:
-#         money = int(input("Enter the amount you wish to play today: "))
-#         while 2000 < money or money < 100:
-#             money = int(input("The deposit doesn't meet our requirement, please input another amount: "))
-#         break  # Break out of the loop if conversion to int is successful
-#
-#     except ValueError:
-#         print("I'm sorry, please input the amount of money you wished to deposit")
-#
-# # while money > 2000 or money < 100:
-# #     money = int(input("The deposit doesn't meet our requirement, please input another amount: "))
-#
-# # If condition to comment on how much they deposit
-# if money < 500:
-#     print("I hope you have a great time at our casino!")
-# else:
-#     print("Feeling lucky today? You are surely one of the high roller in our casino!")
-#
-# game = input("Your deposit is processed. Please choose the games that you would like to start: ")
-
-
-# I will attempt to reproduce the above code as a class object "Player" with attributes "name" and "money"
-
 balance_sheet = {}
 
 
